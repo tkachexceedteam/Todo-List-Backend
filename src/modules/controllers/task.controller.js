1ceae483874ba1af772885d513aaad624280e119
tkachexceedteam <tkach.exceedteam@gmail.com>
2021-06-24 17:27:10 +0300

    some changes for better work

--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -1,42 +1,42 @@
 const tasks = [];
 
 module.exports.getAllTasks = async (req, res, next) => {
-  res.send({data: tasks});
+    res.send({data: tasks});
 };
 
 module.exports.createNewTask = (req, res, next) => {
-  const body = req.body;
-  if (body.hasOwnProperty('text') && body.hasOwnProperty('isCheck')) {
-    body.id = tasks.length + 1;
-    tasks.push(body);
-    res.send({data: tasks});
-  } else {
-    res.status(422).send('Error! Params not correct');
-  }
+    const body = req.body;
+    if (body.hasOwnProperty('text') && body.hasOwnProperty('isCheck')) {
+        body.id = String(Date.now());
+        tasks.push(body);
+        res.send({data: tasks});
+    } else {
+        res.status(422).send('Error! Params not correct');
+    }
 };
 
 module.exports.changeTaskInfo = (req, res, next) => {
-  const body = req.body;
-  if (body.hasOwnProperty('id') && (body.hasOwnProperty('text') || body.hasOwnProperty('isCheck'))) {
-    tasks.forEach((item, i) => {
-      if(item.id === body.id) {
-        for(let key in body) {
-          tasks[i][key] = body[key];
-        }
-      }
-    });
-    res.send({data: tasks});
-  } else {
-    res.status(422).send('Error! Params not correct');
-  }
+    const body = req.body;
+    if (body.hasOwnProperty('id') && (body.hasOwnProperty('text') || body.hasOwnProperty('isCheck'))) {
+        tasks.forEach((item, i) => {
+            if (item.id === body.id) {
+                for (let key in body) {
+                    tasks[i][key] = body[key];
+                }
+            }
+        });
+        res.send({data: tasks});
+    } else {
+        res.status(422).send('Error! Params not correct');
+    }
 };
 
 module.exports.deleteTask = (req, res, next) => {
   if (!req.query.id) return res.status(422).send('Error! Params not correct');
-  const task = tasks.filter(item => item.id === +req.query.id);
+  const task = tasks.filter(item => item.id === req.query.id);
   if (task.length) {
     tasks.forEach((item, i) => {
-      if(item.id === +req.query.id) {
+      if(item.id === req.query.id) {
         tasks.splice(i, 1);
       }
     });
